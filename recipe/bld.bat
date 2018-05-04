7dd3cdea0678229f553e07e7fe1504fbf5cc096a
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-05-03 23:39:48 -0400

    Create some potentially missing directories
    Make sure some basic directories are exist in the prefix (creating them
    if necessary) to avoid build issues caused by their absence.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,15 @@
+if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
+if errorlevel 1 exit 1
+
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
+if errorlevel 1 exit 1
+
+if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
+if errorlevel 1 exit 1
+
 %PYTHON% configure.py --bootstrap
 if errorlevel 1 exit 1
 
