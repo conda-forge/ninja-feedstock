7caaf466d4517e879764f6b7e87e0721fd2e6f7a
Uwe L. Korn <uwe.korn@quantco.com>
2021-09-25 21:18:28 +0200

    Modernize recipe, remove python dep

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,14 @@ mkdir -p "${PREFIX}/include"
 mkdir -p "${PREFIX}/lib"
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    (
+        unset LDFLAGS
+	unset CXXFLAGS
+	export CXX=${CXX_FOR_BUILD}
+
+	./configure.py --bootstrap
+	mv ninja ${BUILD_PREFIX}/bin/
+    )
     ./configure.py
     ninja
 else
