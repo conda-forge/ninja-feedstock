ad4a4148a775bd62ab6681614223bdf8e66cdc29
Uwe L. Korn <xhochy@users.noreply.github.com>
2021-09-25 21:25:55 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,11 +8,11 @@ mkdir -p "${PREFIX}/lib"
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     (
         unset LDFLAGS
-	unset CXXFLAGS
-	export CXX=${CXX_FOR_BUILD}
+        unset CXXFLAGS
+        export CXX=${CXX_FOR_BUILD}
 
-	./configure.py --bootstrap
-	mv ninja ${BUILD_PREFIX}/bin/
+        ./configure.py --bootstrap
+        mv ninja ${BUILD_PREFIX}/bin/
     )
     ./configure.py
     ninja
